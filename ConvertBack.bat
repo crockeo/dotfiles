9256e730f908bbe686f36b3033ccac5fc658b458
Crockeo <crockeo@gmail.com>
2014-08-17 00:11:04 -0400

    Removed the convert scripts.

--- ConvertBack.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-REN "_vimrc" ".vimrc"
-REN "_viminfo" ".viminfo"
-REN "vimfiles" ".vim"
--- ConvertToWindows.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-REN ".vimrc" "_vimrc"
-REN ".viminfo" "_viminfo"
-REN ".vim" "vimfiles"
