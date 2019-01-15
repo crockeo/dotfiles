313f891026a6b13e290e7e72260f38f31227a6c5
Cerek Hillen <cerekh@gmail.com>
2019-01-15 18:42:32 +0000

    Started tracking .gitconfig

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
+cd $(dirname $0)
+
 mkdir -p ~/.config/nvim
 
 ln .zshrc ~/.zshrc
 ln .tmux.conf ~/.tmux.conf
 ln init.vim ~/.config/nvim/init.vim
+ln .gitconfig ~/.gitconfig
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- undeploy.sh
+++ undeploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
+cd $(dirname $0)
+
 rm ~/.tmux.conf
 rm ~/.zshrc
+rm ~/.gitconfig
 
 rm -r ~/.config/nvim
